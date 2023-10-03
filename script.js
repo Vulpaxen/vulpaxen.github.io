946ddb9ca7ba17012a7344525b63fa2c0245f862
Joaquin Arlind <joaquinarlind26@gmail.com>
2023-10-03 16:16:14 +0700

    Adding animation & fixing navbar, adding footer

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
