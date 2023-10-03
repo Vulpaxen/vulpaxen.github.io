64bd83748b1d8d94ad64cbd1a84b6e94d5c646f8
Joaquin Arlind <joaquinarlind26@gmail.com>
2023-10-03 20:08:19 +0700

    Tweaking navbar and logo animation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar appear when scrolled
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     if (window.scrollY > 50) {
@@ -6,3 +7,22 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 });
+
+// Logo Change
+function changeImage() {
+    var image = document.getElementById('logo');
+    image.style.opacity = 0;
+    setTimeout(function() {
+        image.src = 'img/Logo.png';
+        image.style.opacity = 1;
+    }, 150);
+}
+
+function resetImage() {
+    var image = document.getElementById('logo');
+    image.style.opacity = 0;
+    setTimeout(function() {
+        image.src = 'img/LogoInvert.png';
+        image.style.opacity = 1;
+    }, 150);
+}
